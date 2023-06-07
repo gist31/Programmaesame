22fb198925a27f383d05405050b6ad23eae1b7d9
gist31 <135836862+gist31@users.noreply.github.com>
2023-06-07 14:33:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Geben Sie eine Nummer ein:");
+            
+long num = Convert.ToInt32(Console.ReadLine());
+
+List<long> CalcoliInArray = new List<long>();
+
+while (num != 0)
+{
+    CalcoliInArray.Add(num);
+    num--;
+} 
+
+long factorial = 1;
+
+foreach (var CalcoloInArray in CalcoliInArray)
+{
+    factorial *= CalcoloInArray;
+}
+            
+System.Console.WriteLine(factorial);
